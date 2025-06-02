9aee76720889e730771164767323c367cf8f4884
joshuapaschall <joshsellshouses@gmail.com>
2025-06-02 19:28:26 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Get a Cash Offer for Your Home</h1>
-    </div>
-  );
-}
