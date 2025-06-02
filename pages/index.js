257d59d02af79221579285886ca1d9ad0b9f5f6c
joshuapaschall <joshsellshouses@gmail.com>
2025-06-02 19:15:48 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Get a Cash Offer for Your Home</h1>
+    </div>
+  );
+}
