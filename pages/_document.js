99e63fd79cab59fcfeb8b797bd459684ed7c2d05
joshuapaschall <joshsellshouses@gmail.com>
2025-06-02 21:26:03 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,17 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Minimal head, no extra tags to block rendering */}
+        <meta charSet="UTF-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
